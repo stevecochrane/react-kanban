6599c2521c8301ef230d9e2484a295b831acd5c1
Steve Cochrane <steve@stevecochrane.com>
2018-01-25 22:35:20 -0600

    Added the Notes component to the index render function.

--- app/index.jsx
+++ app/index.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import Notes from "./components/Notes";
 
-if(process.env.NODE_ENV !== 'production') {
-  React.Perf = require('react-addons-perf');
+if (process.env.NODE_ENV !== "production") {
+	React.Perf = require("react-addons-perf");
 }
 
 ReactDOM.render(
-  <div>Hello world</div>,
-  document.getElementById('app')
+	<Notes />,
+	document.getElementById("app")
 );
