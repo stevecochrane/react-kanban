21a4941929fde26fdbf3f80b2eeef71e534d7177
Steve Cochrane <steve@stevecochrane.com>
2018-01-25 22:53:11 -0600

    Moved notes data from Notes to App.

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,4 +1,21 @@
 import React from "react";
+import uuid from "uuid";
 import Notes from "./Notes";
 
-export default () => <Notes />;
+const notes = [
+	{
+		id: uuid.v4(),
+		task: "Learn React"
+	},
+	{
+		id: uuid.v4(),
+		task: "Do laundry"
+	}
+];
+
+export default () => (
+	<div>
+		<button onClick={() => console.log("add note")}>+</button>
+		<Notes notes={notes} />
+	</div>
+);
