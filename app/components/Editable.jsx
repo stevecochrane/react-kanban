43edd5873e782b0fc496b572606f6494f4b72152
Steve Cochrane <steve@stevecochrane.com>
2018-01-27 17:18:01 -0600

    Added note editing functionality to Editable and App.

--- app/components/Editable.jsx
+++ app/components/Editable.jsx
@@ -4,11 +4,30 @@ export default ({editing, value, onEdit, ...props}) => {
 	if (editing) {
 		return <Edit value={value} onEdit={onEdit} {...props} />;
 	}
-	return <span {...props}>value: {value}</span>;
+	return <span {...props}>{value}</span>;
 };
 
-const Edit = ({onEdit = () => {}, value, ...props}) => (
-	<div onClick={onEdit} {...props}>
-		<span>edit: {value}</span>
-	</div>
-);
+class Edit extends React.Component {
+	render() {
+		const {value, onEdit, ...props} = this.props;
+		return <input
+			type="text"
+			autoFocus={true}
+			defaultValue={value}
+			onBlur={this.finishEdit}
+			onKeyPress={this.checkEnter}
+			{...props}
+			/>;
+	}
+	checkEnter = (e) => {
+		if (e.key === "Enter") {
+			this.finishEdit(e);
+		}
+	}
+	finishEdit = (e) => {
+		const value = e.target.value;
+		if (this.props.onEdit) {
+			this.props.onEdit(value);
+		}
+	}
+}
