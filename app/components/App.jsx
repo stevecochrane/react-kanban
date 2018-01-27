d5067861cd3410a3f8cd91ca814239d09a83adc2
Steve Cochrane <steve@stevecochrane.com>
2018-01-27 16:21:56 -0600

    Integrated note deletion with Notes and App.

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -25,7 +25,7 @@ export default class App extends React.Component {
 		return (
 			<div>
 				<button onClick={this.addNote}>+</button>
-				<Notes notes={notes} />
+				<Notes notes={notes} onDelete={this.deleteNote} />
 			</div>
 		);
 	}
@@ -36,5 +36,11 @@ export default class App extends React.Component {
 				task: "New task"
 			}])
 		})
+	}
+	deleteNote = (id, e) => {
+		e.stopPropagation();
+		this.setState({
+			notes: this.state.notes.filter(note => note.id !== id)
+		});
 	}
 }
