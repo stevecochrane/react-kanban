8fa9f3c399ade9c38a44271c73e8d13912ded540
Steve Cochrane <steve@stevecochrane.com>
2018-01-27 13:09:17 -0600

    Added a stub for a new Note component.

--- app/components/Notes.jsx
+++ app/components/Notes.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import Note from "./Note";
 
 export default ({notes}) => (
 	<ul>{notes.map(note =>
-		<li key={note.id}>{note.task}</li>
+		<li key={note.id}><Note task={note.task} /></li>
 	)}</ul>
 );
