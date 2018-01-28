42091caf4fc374e695e75794612ab4f1e42a6368
Steve Cochrane <steve@stevecochrane.com>
2018-01-28 17:25:04 -0500

    Added a Connect class with test data output to App.

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import uuid from "uuid";
 import Notes from "./Notes";
+import connect from "../libs/connect";
 
-export default class App extends React.Component {
+class App extends React.Component {
 	constructor(props) {
 		super(props);
 
@@ -24,6 +25,7 @@ export default class App extends React.Component {
 
 		return (
 			<div>
+				{this.props.test}
 				<button className="add-note" onClick={this.addNote}>+</button>
 				<Notes
 					notes={notes}
@@ -70,3 +72,7 @@ export default class App extends React.Component {
 		});
 	}
 }
+
+export default connect(() => ({
+	test: "test"
+}))(App);
