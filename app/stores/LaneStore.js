2ee345cffe2c0eff5bafa1e34d8aab36ad3e361c
Steve Cochrane <steve@stevecochrane.com>
2018-02-03 17:09:24 -0600

    Added a new Lane action for detaching a Note from a Lane.

--- app/stores/LaneStore.js
+++ app/stores/LaneStore.js
@@ -24,4 +24,14 @@ export default class LaneStore {
 			})
 		});
 	}
+	detachFromLane({laneId, noteId}) {
+		this.setState({
+			lanes: this.lanes.map(lane => {
+				if (lane.id === laneId) {
+					lane.notes = lane.notes.filter(note => note !== noteId);
+				}
+				return lane;
+			})
+		});
+	}
 }
