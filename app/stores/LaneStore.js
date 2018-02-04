b1822ba1267c4a1b0c0d99e1c049ad0df4740b70
Steve Cochrane <steve@stevecochrane.com>
2018-02-04 14:09:30 -0600

    Dragging and dropping now mostly works.

--- app/stores/LaneStore.js
+++ app/stores/LaneStore.js
@@ -1,3 +1,4 @@
+import update from "react-addons-update";
 import LaneActions from "../actions/LaneActions";
 
 export default class LaneStore {
@@ -50,6 +51,24 @@ export default class LaneStore {
 		});
 	}
 	move({sourceId, targetId}) {
-		console.log(`source: ${sourceId}, target: ${targetId}`);
+		const lanes = this.lanes;
+		const sourceLane = lanes.filter(lane => lane.notes.includes(sourceId))[0];
+		const targetLane = lanes.filter(lane => lane.notes.includes(targetId))[0];
+		const sourceNoteIndex = sourceLane.notes.indexOf(sourceId);
+		const targetNoteIndex = targetLane.notes.indexOf(targetId);
+
+		if (sourceLane === targetLane) {
+			sourceLane.notes = update(sourceLane.notes, {
+				$splice: [
+					[sourceNoteIndex, 1],
+					[targetNoteIndex, 0, sourceId]
+				]
+			});
+		} else {
+			sourceLane.notes.splice(sourceNoteIndex, 1);
+			targetLane.notes.splice(targetNoteIndex, 0, sourceId);
+		}
+
+		this.setState({lanes});
 	}
 }
